df80ddc5aea4475c165eeb334281875406728976
giovannidemaria <giovidemaria@gmail.com>
2023-06-16 21:58:04 +0200

    update

--- src/Polynomial.java
+++ src/Polynomial.java
@@ -15,6 +15,33 @@ public class Polynomial implements RealFunction {
     return result;
   }
 
+  @Override
+  public RealFunction derivative() {
+    double[] coefficients = getCoefficients();
+    int grade = coefficients.length;
+    double[] coefficientsDerivative = new double[grade - 1];
+    for (int i = 0; i < grade - 1; i++) {
+      coefficientsDerivative[i] = coefficients[i + 1] * (i + 1);
+    }
+    return new Polynomial(coefficientsDerivative);
+  }
+
+  @Override
+  public RealFunction integrate(double x0, double step) {
+    double[] coefficients = getCoefficients();
+    int grade = coefficients.length;
+    double[] coefficientsIntegral = new double[grade + 1];
+    for (int i = 1; i < grade + 1; i++) {
+      coefficientsIntegral[i] = coefficients[i - 1] / i;
+    }
+
+    Polynomial indefiniteIntegral = new Polynomial(coefficientsIntegral);
+
+    double startval = indefiniteIntegral.apply(x0);
+    coefficientsIntegral[0] -= startval;
+    return new Polynomial(coefficientsIntegral);
+  }
+
   public double[] getCoefficients() {
     return coefficients;
   }
